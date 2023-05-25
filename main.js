180fb1e4ef5394bd4925d7c1f946376420e6bc93
blaztted <bbc.silva@gmail.com>
2023-05-25 19:35:01 +0100

    Build CSS and start building form with HTML and JS

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('teste');
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read; 
+}
+
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").value;
+    let newBook = new Book(title, author, pages, read);
+    console.log(newBook);
+}
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function(){
+    let newBookform = document.querySelector("#new-book-form");
+    newBookform.style.display = "block";
+
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function(){
+    event.preventDefault();
+    addBookToLibrary();
+})
